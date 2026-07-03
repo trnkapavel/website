472f2967b893b26be98506aba71806d2cdfd4bca
Pavel Trnka <trnkapavel@gmail.com>
2026-07-03 22:21:34 +0200

    feat: scaffold Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://example.com', // TODO při nasazení: nahradit skutečnou doménou (jediný povolený TODO v projektu)
+});
